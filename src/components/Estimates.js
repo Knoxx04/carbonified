7cf19d9806afc454c9f9bed0e5c18d55359cd745
Knoxx04 <onyiknoxx3@gmail.com>
2023-01-26 23:24:03 +0300

    Flights.js done

--- src/components/Estimates.js
+++ src/components/Estimates.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Electricity from './Electricity'
+import Flights from './Flights'
 
 function Estimates() {
   return (
     <div>
       <Electricity/>
+      <Flights/>
       </div>
   )
 }
---
+++ src/components/Flights.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+
+const Flights = () => {
+  const [response, setResponse] = useState({});
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  } 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const requestBody = {
+        
+            type: "flight",
+            passengers: 2,
+            legs: [
+              {"departure_airport": "", "destination_airport": ""},
+              {"departure_airport": "", "destination_airport": ""}
+            ]
+          
+    };
+
+    try {
+      const res = await fetch('', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer YQytsSidKitpSJj1eNbGsw`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+      const data = await res.json();
+      setResponse(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+
+  return(
+    <div className="">
+        <div className="">
+            <h4 className="">Enter the destination to estimate</h4>
+            <form className="" onSubmit={handleSubmit}>
+                <input className="" type="number" value={inputValue} onChange={handleChange}/>
+                <button className="" type="submit">Estimate</button>
+            </form>
+        </div>
+
+        {response.data && response.data.attributes && <p>Carbon Emission: {response.data.attributes.carbon_kg} kg</p>}
+        
+    </div>
+  )
+}
+
+
+export default Flights
