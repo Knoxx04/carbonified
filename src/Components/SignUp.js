c7e6fa5dae8b361f9b91b8045afe86aad95ad6c1
TimothyKagondu <timothy.2k21@gmail.com>
2023-01-26 11:52:04 +0300

    navigation to home button

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -19,6 +19,15 @@ function SignUp () {
                         <i className="fas fa-phone"></i>
                         <p>Tel: +254 7200001001</p>
                     </div>
+                    <div className="contact">
+                        <i className="fas fa-envelope"></i>
+                        <p>Email: support@Carbonified.com</p>
+                    </div>
+                </div>
+                <div className="navigation">
+                    <a href="./index.html">Back to home</a>
+                    <i className="fas fa-arrow-left"></i>
+                </div>
     );
 }
 
