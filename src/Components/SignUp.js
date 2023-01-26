f6de6a6c303dd8948b6befdcd9c458ea6a13d256
TimothyKagondu <timothy.2k21@gmail.com>
2023-01-26 11:46:29 +0300

    contact on login form

---
+++ src/Components/SignUp.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function SignUp () {
+
+    return (
+         
+        <div className="hero">
+        <div className="form-box left">
+            <div class="info">
+                <h2>Carbonified</h2>
+                <div className="contact-info">
+                    <p>box 300 - 00100,</p>
+                    <p>Nairobi,</p>
+                    <p>Kenya</p>
+                </div>
+
+    );
+}
+
+export default SignUp;
